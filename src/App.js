bb1100ce8bf3a44f90856678759053e412d67a4e
thomasSDK <tsdkelly@protonmail.com>
2020-07-15 10:49:57 +0100

    basic api call created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from 'react';
+const superagent = require('superagent');
+
+const clientID = "eK3hqxpfSE979oi91j3VGsUERwEEj3YvGIOifB_5hfE";
+
+const simpleGet = options => {
+  superagent.get(options.url).then(function(res) {
+    if (options.onSuccess) options.onSuccess(res);
+  });
+};
+
+const url = "https://api.unsplash.com/photos/random/?count=1&query=sport&client_id="
 
 function App() {
+  const [photo, setPhoto] = useState({})
+  
+  useEffect(() => {
+    const photosUrl = `${url}${clientID}`
+
+    simpleGet({
+      url: photosUrl,
+      onSuccess: res => {
+        setPhoto(res.body);
+      }
+    });
+    console.log(photo)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
