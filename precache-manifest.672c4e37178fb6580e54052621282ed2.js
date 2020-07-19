1bbe29bdd47a19ead10276952bd61d7de6e0cb62
thomasSDK <tsdkelly@protonmail.com>
2020-07-19 16:17:20 +0100

    Updates

---
+++ precache-manifest.672c4e37178fb6580e54052621282ed2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0c3fdba3d34b8dd915b9fea6ea1241",
+    "url": "/sports-paparazzi/index.html"
+  },
+  {
+    "revision": "91a94d12d5e44fb74070",
+    "url": "/sports-paparazzi/static/css/main.f6303228.chunk.css"
+  },
+  {
+    "revision": "2121b8db90a8e03cecb6",
+    "url": "/sports-paparazzi/static/js/2.745af768.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sports-paparazzi/static/js/2.745af768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a94d12d5e44fb74070",
+    "url": "/sports-paparazzi/static/js/main.9b939ed1.chunk.js"
+  },
+  {
+    "revision": "3dd141f0a890597d3580",
+    "url": "/sports-paparazzi/static/js/runtime-main.9d5239c4.js"
+  },
+  {
+    "revision": "48419a412ae68ffb881ddb07b9d7e274",
+    "url": "/sports-paparazzi/static/media/Moonhouse.48419a41.ttf"
+  }
+]);
