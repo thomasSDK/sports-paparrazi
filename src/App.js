c1e4e02db03e8d5a79f429262f0ab68684a47e41
thomasSDK <tsdkelly@protonmail.com>
2020-07-19 11:01:30 +0100

    tided api request

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
-import React, { useState, useEffect} from 'react';
-const superagent = require('superagent');
+import React, { useState, useEffect } from "react";
+import styled from 'styled-components'
+const superagent = require("superagent");
 
+// Split  to allow for greater flexibility in the request
+const url =
+  "https://api.unsplash.com/photos/random/?count=1&query=sport&orientation=portrait&client_id=";
 const clientID = "eK3hqxpfSE979oi91j3VGsUERwEEj3YvGIOifB_5hfE";
 
-const simpleGet = options => {
-  superagent.get(options.url).then(function(res) {
+const simpleGet = (options) => {
+  superagent.get(options.url).then(function (res) {
     if (options.onSuccess) options.onSuccess(res);
   });
 };
 
-const url = "https://api.unsplash.com/photos/random/?count=1&query=sport&client_id="
+const photosUrl = `${url}${clientID}`;
 
 function App() {
-  const [photo, setPhoto] = useState({})
-  
-  useEffect(() => {
-    const photosUrl = `${url}${clientID}`
+  const [photos, setPhotos] = useState([]);
 
+  useEffect(() => {
     simpleGet({
       url: photosUrl,
-      onSuccess: res => {
-        setPhoto(res.body);
-      }
+      // returns an array of photos
+      onSuccess: (res) => {
+        setPhotos(res.body);
+        console.log(res.body)
+      },
     });
-    console.log(photo)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-  
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="App">
